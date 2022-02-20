4600a796550fd1fb31859739520eea239ad27337
squ3dy <79893035+squ3dy@users.noreply.github.com>
2022-02-20 09:55:02 +0400

    Add files via upload

--- ip_checker.py
+++ ip_checker.py
@@ -9,13 +9,22 @@ import logging
 @loader.tds
 class ip_checkerMod(loader.Module):
     """ip check module"""
-    
+
     async def ipcmd(self, message: types.Message):
-        """just simple ip checker, type .ip <here ip adress> to check location."""
+        """type .ip <here ip adress> to check location."""
         args = utils.get_args_raw(message)
         ip = str(args.split(' ', 2)[0])
         response = urllib.request.urlopen("http://ipwhois.app/json/"+ip)
         ipgeolocation = json.load(response)
-        await message.client.send_message(message.to_id, ipgeolocation["country_code"] )
+        await message.client.send_message(message.to_id, 'country - ' + ipgeolocation["country"] + '\ncity - ' + ipgeolocation["city"])
+    
+    async def remaincmd(self, message: types.Message):
+        """type .remain to check remaining requests"""
+        response = urllib.request.urlopen("http://ipwhois.app/json/")
+        ipgeolocation = json.load(response)
+        requests = 10000 - int(ipgeolocation["completed_requests"])
+        await message.client.send_message(message.to_id, 'remaining requests - ' + str(requests))
+
+
     
 
